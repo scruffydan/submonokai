294fd3901e041b5b4725b1c05f6c30c8365dfc02
Dan Moutal <dan@moutal.ca>
2018-12-30 13:21:07 -0800

    initial commit

---
+++ SubMonokai Colors.md
@@ -0,0 +1,18 @@
+# SubMonokai Colors:
+
+* Background: #272822
+* Foreground (text): #F8F8F2
+* Selection: #49483E
+* Comments: #75715E
+* Sidebar: #171714
+
+* Red: #ED3A2C
+* Orange: #FF9800
+* Yellow: #E6DC6D
+* Magenta: #FB2B71
+* Green: #B3E435
+* Light Blue: #00DFF3
+* Blue: #0097F3
+* White: #F8F8F2
+* Grey: #808080
+* Black: #1D1E19
---
+++ SubMonokai-Blink.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#1D1E19", "#ED3A2C", "#E6DC6D", "#FF9800", "#0097F3", "#FB2B71", "#00DFF3", "#F6EDEE", "#E0D75A", "#ED3A2C", "#E6DC6D", "#FF9800", "#0097F3", "#FB2B71", "#00DFF3", "#F6EDEE"]);
+t.prefs_.set('foreground-color', "#F8F8F2");
+t.prefs_.set('background-color', "#272822");
+t.prefs_.set('cursor-color', 'rgba(248, 248, 242,0.5)');
